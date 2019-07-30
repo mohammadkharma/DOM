236d76f561212e3364d9f0a2085785179979b76c
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-07-31 01:00:31 +0200

    created a chatroom class and add chat method in chat.js

---
+++ projects/real_time_chatroom/scripts/chat.js
@@ -0,0 +1,35 @@
+// Page content:
+// Adding new chat documents.
+// Setting up a real time listener.
+// Watching the username.
+// Updating the room.
+
+class Chatroom {
+    constructor(room, username) {
+        this.room = room,
+            this.username = username,
+            this.chats = db.collection('chats')
+    }
+
+    // format a chat object
+    async addChat(message) {
+        const now = new Date();
+        const chat = {
+            message: message,
+            room: this.room,
+            username: this.username,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        }
+
+        // save the chat document
+        const response = await this.chats.add(chat);
+        return response;
+    }
+}
+
+const chatroom = new Chatroom('gaming', 'Mo');
+chatroom.addChat("hello everyone!")
+    .then(() => console.log('chat added'))
+    .catch(error => {
+        console.log(error)
+    });
