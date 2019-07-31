cdeb74a7e6c7b85539cd76622cc36225df2da587
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-07-31 22:40:36 +0200

    did the updateName on the UI

--- projects/real_time_chatroom/scripts/app.js
+++ projects/real_time_chatroom/scripts/app.js
@@ -1,19 +1,33 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMsg = document.querySelector('.update-msg');
 
 // add new chat
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = newChatForm.message.value.trim();
     chatroom.addChat(message)
-    .then(() => newChatForm.reset())
-    .catch(error => console.log(error));
+        .then(() => newChatForm.reset())
+        .catch(error => console.log(error));
 });
 
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'Mo');
+
+// adding new name
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    console.log(newName);
+    chatroom.updateName(newName);
+    newNameForm.reset();
+    updateMsg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMsg.innerText = '', 3000);
+});
+
 
 
 // get chats and render
