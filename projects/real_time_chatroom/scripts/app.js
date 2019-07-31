ac643aad0f25c539b9a32c378c67751ff2f6a880
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-07-31 03:49:04 +0200

    managed the add chat input

--- projects/real_time_chatroom/scripts/app.js
+++ projects/real_time_chatroom/scripts/app.js
@@ -1,10 +1,20 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// add new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(error => console.log(error));
+});
 
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'Mo');
 
 
-// Get chats and render
+// get chats and render
 chatroom.getChats(data => chatUI.render(data));
