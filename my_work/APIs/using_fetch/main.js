f642eb3c2960c939d42ee1babc0ae91302c2f2c8
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-01 17:39:13 +0200

    practicing local APIs

--- my_work/APIs/using_fetch/main.js
+++ my_work/APIs/using_fetch/main.js
@@ -1,7 +1,7 @@
 var verseChoose = document.querySelector('select');
 var poemDisplay = document.querySelector('pre');
 
-verseChoose.onchange = function() {
+verseChoose.onchange = function () {
   var verse = verseChoose.value;
   updateDisplay(verse);
 };
@@ -10,14 +10,12 @@ function updateDisplay(verse) {
   verse = verse.replace(" ", "");
   verse = verse.toLowerCase();
   var url = verse + '.txt';
+  fetch(url).then(function(response) {
+    response.text().then(function(text) {
+      poemDisplay.textContent = text;
+    });
+  });
 };
 
-var request = new XMLHttpRequest();
-
-request.open('GET', url);
-
-request.responseType = 'text';
-
-request.onload = function() {
-  poemDisplay.textContent = request.response;
-};
+updateDisplay('Verse 1');
+verseChoose.value = 'Verse 1';
