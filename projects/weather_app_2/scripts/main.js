ccb3d34573196c52e7198defc89a83c58d91b684
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-03 16:25:27 +0200

    the city weather conditions are available

--- projects/weather_app_2/scripts/main.js
+++ projects/weather_app_2/scripts/main.js
@@ -8,7 +8,44 @@ toggleState();
 console.log('main.js is ready');
 // ------------------------------------------------------- //
 
-const API_key = "uLcGCTGBk2tvfKmztREtgRCGXG1U17sG";
-fetch(API_key).then(r => {
-    console.log(r);
-}).catch(err => console.log(err))
+const mainKey = "L6TYjZMGAbuxAAvUPFXzRoPiZZKq7ZEt";
+
+const getCity = async cityName => {
+
+    const mainURL = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${mainKey}&q=${cityName}`;
+
+    const response = await fetch(mainURL+query);
+    const data = await response.json();
+
+    console.log('getCity', data[0]);
+    return data[0];
+}
+
+getCity('berlin');
+
+const getConditions = async (cityKey) => {
+
+
+    const mainURL = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${cityKey}?apikey=${mainKey}`;
+
+    const response = await fetch(mainURL+query);
+    const data = await response.json();
+
+    console.log('getConditions', data[0]);
+    return data[0];
+}
+
+
+const updateCity = async city => {
+
+const cityDetails = await getCity(city);
+const cityWeatherConditions = await getConditions(cityDetails.Key);
+
+const merged = {cityDetails, cityWeatherConditions};
+console.log(merged);
+
+}
+
+updateCity('berlin');
